846cf89261a61ff71f7b29106300d44e54a9e0ee
Wiellona <wiellonadarlene@gmail.com>
2023-12-11 23:48:55 +0700

    benerin tp ttp gajelas

--- main2.c
+++ main2.c
@@ -6,13 +6,9 @@
 struct roomLighting
 {
     float panjang, lebar; // panjang dan lebar ruangan
-    float penerangan;   // Pencahayaan yang diinginkan (dalam lux)
-    float rW;   // reflektansi dinding ruangan
-    float rH;   // tinggi ruangan
-    float wH;   // tinggi area kerja
-    float RI;   // room index
-    float UF;   // utilization factor
-    float N;    // Banyak lampu yang dibutuhkan
+    float penerangan;     // Pencahayaan yang diinginkan (dalam lux)
+    float UF;             // utilization factor
+    float N;              // Banyak lampu yang dibutuhkan
     void (*setData)(struct roomLighting *ruangan);
     void (*hitungRI)(struct roomLighting *ruangan);
     void (*hitungN)(struct roomLighting *ruangan);
@@ -42,13 +38,10 @@ void utilizationFactor()
     printf("| 4.0 | 0.72| 0.70| 0.68|| 0.70| 0.69| 0.67|| 0.67| 0.66| 0.64||   0.63 |   0.61 ||   0.58|\n");
     printf("| 5.0 | 0.73| 0.71| 0.70|| 0.71| 0.70| 0.68|| 0.68| 0.67| 0.66||   0.64 |   0.63 ||   0.59|\n");
     printf("===========================================================================================\n");
-    printf("!--- Tekan ENTER untuk kembali ke menu utamaa ---!");
-    getchar();
-    system("CLS");
-    menu();
 }
 
-int main () {
+int main(void)
+{
     struct roomLighting *ruangan = (struct roomLighting *)malloc(sizeof(struct roomLighting));
     int choice, n = 1;
     float watt;
@@ -60,17 +53,21 @@ int main () {
 
     switch (choice)
     {
-        case 1:
-            hitungLampu(ruangan, &watt);
-            hitungN(ruangan, &watt);
-            break;
-        case 2:
-            dataRuang();
-            break;
-        case 3:
-            keluar();
-        default:
-            break;
+    case 1:
+        hitungLampu(ruangan, &watt);
+        hitungN(ruangan, &watt);
+        break;
+    case 2:
+        dataRuang();
+        break;
+    case 3:
+        utilizationFactor();
+        break;
+    case 4:
+        keluar();
+        break;
+    default:
+        break;
     }
 }
 
@@ -89,17 +86,13 @@ void menu()
            "|     dibutuhkan                        |\n"
            "|  2. Data kebutuhan lampu tiap ruang   |\n"
            "|  3. Utilization Factor Table          |\n"
-           "|  3. Keluar                            |\n"
+           "|  4. Keluar                            |\n"
            "=========================================\n");
-    printf("!--- Tekan ENTER untuk kembali ke menu utamaa ---!");
-    getchar();
-    system("CLS");
-    menu();
 }
 
-void hitungLampu(struct roomLighting *ruangan, float *watt) 
+void hitungLampu(struct roomLighting *ruangan, float *watt)
 {
-    int n, i;
+    char respon;
     printf("===========================================\n");
     printf("| Menghitung Banyak Lampu yang Dibutuhkan |\n");
     printf("|           dan Komparasi Lampu           |\n");
@@ -108,36 +101,38 @@ void hitungLampu(struct roomLighting *ruangan, float *watt)
     scanf("%f\n", &ruangan->panjang);
     printf(" Masukkan lebar ruangan : ");
     scanf("%f\n", &ruangan->lebar);
-    printf(" Masukkan tinggi ruangan : ");
-    scanf("%f\n", &ruangan->rH);
-    printf(" Masukkan tinggi area kerja : ");
-    scanf("%f\n", &ruangan->wH);
     printf(" Pencahayaan yang dibutuhkan : ");
     scanf("%f\n", &ruangan->penerangan);
 
-    do
+    printf("Apakah Anda mengetahui nilai UF dari ruangan? Ketik 'YA' atau 'TIDAK'\n");
+    scanf("%s\n", &respon);
+
+    if (respon == 'YA')
     {
-        printf(" Reflektansi dinding ruangan (Pilih salah satu : 0.1 / 0.3 / 0.5) : ");
-        scanf("%f\n", &ruangan->rW);
-    } while (ruangan->rW != 0.1 || ruangan->rW != 0.1 || ruangan->rW != 0.1);
+        utilizationFactor();
+        printf(" Utilization Factor ruangan : ");
+        scanf("%f\n", &ruangan->UF);
+    }
+    else
+    {
+        printf(" Utilization Factor ruangan : ");
+        scanf("%f\n", &ruangan->UF);
+    }
 
     printf(" Masukkan besar daya lampu : ");
     scanf("%f\n", watt);
 }
 
-void hitungN(struct roomLighting *ruangan, float *watt) 
+void hitungN(struct roomLighting *ruangan, float *watt)
 {
     ruangan->N = (ruangan->penerangan * ruangan->panjang * ruangan->lebar) / (*watt * 10 * ruangan->UF * 0.75);
     roundf(ruangan->N); // lampu->watt * 10 untuk mengoversi terang lampu dari lumen ke watt
 
     printf(" Banyak lampu yang perlu Anda beli adalah sebanyak %.2f buah.\n", roundf(ruangan->N));
-    printf("!--- Tekan ENTER untuk kembali ke menu utamaa ---!");
-    getchar();
-    system("CLS");
-    menu();
 }
 
-void dataRuang() {
+void dataRuang()
+{
     printf("===========================================\n");
     printf("|    Data Kebutuhan Lampu Tiap Ruangan    |\n");
     printf("===========================================\n");
@@ -149,52 +144,31 @@ void dataRuang() {
     printf("|      Kamar mandi      |       200 lux   |\n");
     printf("|      Ruang keluarga   |       150 lux   |\n");
     printf("===========================================\n");
-    printf("!--- Tekan ENTER untuk kembali ke menu utamaa ---!");
-    getchar();
-    system("CLS");
-    menu();
 }
 
 void masuk()
 {
+    printf("======================================\n"
+           "|     Selamat Datang Di Aplikasi     |\n"
+           "|     Lamp Efficiency Calculator     |\n"
+           "|             Kelompok 9             |\n"
+           "|        Pemrograman Dasar 01        |\n"
+           "======================================\n");
     int i;
-    printf("!--- Harap Fullscreen lalu tekan ENTER untuk memulai aplikasi ---!");
+    printf("!--- Tekan ENTER untuk memulai aplikasi ---!");
     getchar();
-    system("CLS"); // Membersihkan layar
-    sleep(1);      // Menjeda program selama 1 detik
-    printf("\n\n\n\n\n\n\n");
-    char a[] = {"\t\t\t\t\t\t\t\t======================================\n"
-                "\t\t\t\t\t\t\t\t|---- Selamat Datang Di Aplikasi ----|\n"
-                "\t\t\t\t\t\t\t\t|---- Lamp Efficiency Calculator ----|\n"
-                "\t\t\t\t\t\t\t\t|------------ Kelompok 9 ------------|\n"
-                "\t\t\t\t\t\t\t\t|------- Pemrograman Dasar 01 -------|\n"
-                "\t\t\t\t\t\t\t\t======================================\n\n"};
-    for (i = 0; i < strlen(a); i++)
-    {
-        printf("%c", a[i]);
-        sleep(1);
-    }
-    printf("\t\t\t\t\t\t\t\t\t\tLoading... \n\n");
     sleep(1);
-    for (i = 1; i <= 172; i++)
-    {
-        printf("%c", 223);
-        if (i == 60 || i == 100)
-            sleep(1);
-        sleep(0.1);
-    }
-    sleep(1.6);
     system("CLS"); // Membersihkan layar
 }
 
 void keluar()
 {
     system("CLS"); // Membersihkan layar
-    sleep(0.1);
+    sleep(1);
     printf("\n\n\n\n\n\n\n");
     printf("\t\t\t\t\t\t\t\t======================================\n");
     printf("\t\t\t\t\t\t\t\t|~ ~ ~ ~ ~ ~ ~ ~ Keluar ~ ~ ~ ~ ~ ~ ~ ~|\n");
     printf("\t\t\t\t\t\t\t\t======================================\n\n");
-    sleep(1600);   // menjeda program selama 1.6 detik
+    sleep(1);    // menjeda program selama 1.6 detik
     system("CLS"); // Membersihkan layar
 }
